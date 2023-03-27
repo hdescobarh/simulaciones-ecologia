f2fd540768b88a46760d7922d0526871fee98227
Hans Escobar <hdescobarh@unal.edu.co>
2023-03-26 22:57:12 -0500

    Implemented Lincoln-Petersen capture-mark-recapture abundance estimator and its standard error calculation. Added a validator class.

--- popecology/abundance_estimation_methods.py
+++ popecology/abundance_estimation_methods.py
@@ -1,4 +1,104 @@
 # python3
 # author: Hans D. Escobar. H - e-mail: escobar.hans@gmail.com
 
-# TODO: implement class and their class methods. One by each technique.
+from numpy import sqrt
+
+
+class LincolnPetersen:
+    estimator_id: str = "abundance"
+    standard_error_id: str = "sd_error"
+
+    @staticmethod
+    def __bailey_unbiased_statistic(
+        captured: int, recaptured_unmarked: int, recaptured_marked: int
+    ) -> float:
+        return (
+            (captured)
+            * (recaptured_unmarked + recaptured_marked + 1)
+            / (recaptured_marked + 1)
+        )
+
+    @staticmethod
+    def __chapman_unbiased_statistic(
+        captured: int, recaptured_unmarked: int, recaptured_marked: int
+    ) -> float:
+        return (
+            (captured + 1)
+            * (recaptured_unmarked + recaptured_marked + 1)
+            / (recaptured_marked + 1)
+        ) - 1
+
+    @staticmethod
+    def __bailey_standard_error(
+        captured: int, recaptured_unmarked: int, recaptured_marked: int
+    ) -> float:
+        variance_numerator = (
+            (captured**2)
+            * (recaptured_unmarked + recaptured_marked + 1)
+            * (recaptured_unmarked)
+        )
+        variance_denominator = (recaptured_marked + 1) ** 2 * (recaptured_marked + 2)
+        return sqrt(variance_numerator / variance_denominator)
+
+    @staticmethod
+    def __chapman_standard_error(
+        captured: int, recaptured_unmarked: int, recaptured_marked: int
+    ) -> float:
+        variance_numerator = (
+            (captured + 1)
+            * (recaptured_unmarked + recaptured_marked + 1)
+            * (captured - recaptured_marked)
+            * recaptured_unmarked
+        )
+        variance_denominator = (recaptured_marked + 1) ** 2 * (recaptured_marked + 2)
+        return sqrt(variance_numerator / variance_denominator)
+
+    @staticmethod
+    def simple_biased_statistic(
+        captured: int, recaptured_unmarked: int, recaptured_marked: int
+    ) -> float:
+        Validator.check_positive_integer(
+            [captured, recaptured_unmarked, recaptured_marked]
+        )
+
+        return captured * (recaptured_unmarked + recaptured_marked) / recaptured_marked
+
+    @staticmethod
+    def bailey_unbiased_summary(
+        captured: int, recaptured_unmarked: int, recaptured_marked: int
+    ) -> dict[str, float]:
+        Validator.check_positive_integer(
+            [captured, recaptured_unmarked, recaptured_marked]
+        )
+        return {
+            LincolnPetersen.estimator_id: LincolnPetersen.__bailey_unbiased_statistic(
+                captured, recaptured_unmarked, recaptured_marked
+            ),
+            LincolnPetersen.standard_error_id: LincolnPetersen.__bailey_standard_error(
+                captured, recaptured_unmarked, recaptured_marked
+            ),
+        }
+
+    @staticmethod
+    def chapman_unbiased_summary(
+        captured: int, recaptured_unmarked: int, recaptured_marked: int
+    ) -> dict[str, float]:
+        Validator.check_positive_integer(
+            [captured, recaptured_unmarked, recaptured_marked]
+        )
+        return {
+            LincolnPetersen.estimator_id: LincolnPetersen.__chapman_unbiased_statistic(
+                captured, recaptured_unmarked, recaptured_marked
+            ),
+            LincolnPetersen.standard_error_id: LincolnPetersen.__chapman_standard_error(
+                captured, recaptured_unmarked, recaptured_marked
+            ),
+        }
+
+
+class Validator:
+    @staticmethod
+    def check_positive_integer(values: list[int]):
+        for v in values:
+            if v <= 0:
+                raise Exception("All values must be positive integers")
--- tests/test_abundance_estimation_methods.py
+++ tests/test_abundance_estimation_methods.py
@@ -1,1 +1,59 @@
-# TODO
+# python3
+# author: Hans D. Escobar. H - e-mail: escobar.hans@gmail.com
+
+from popecology import abundance_estimation_methods as aem
+import pytest
+
+# Validator
+
+
+def test_validator_check_positive_integer_raise_exception():
+    with pytest.raises(Exception):
+        aem.Validator.check_positive_integer([1, 2, -5])
+
+
+# LincolnPetersen
+
+
+def test_LincolnPetersen_simple():
+    captured: int = 87
+    recaptured_unmarked: int = 7
+    recaptured_marked: int = 7
+    estimator = aem.LincolnPetersen.simple_biased_statistic(
+        captured, recaptured_unmarked, recaptured_marked
+    )
+    assert abs(estimator - 174) <= 0.0000000005
+
+
+def test_LincolnPetersen_bailey():
+    captured: int = 87
+    recaptured_unmarked: int = 7
+    recaptured_marked: int = 7
+
+    bailey_summary = aem.LincolnPetersen.bailey_unbiased_summary(
+        captured, recaptured_unmarked, recaptured_marked
+    )
+
+    assert (
+        abs(bailey_summary[aem.LincolnPetersen.estimator_id] - 163.125) <= 0.0000000005
+    )
+    assert (
+        abs(bailey_summary[aem.LincolnPetersen.standard_error_id] - 37.1451965266)
+        <= 0.0000000005
+    )
+
+
+def test_LincolnPetersen_chapman():
+    captured: int = 87
+    recaptured_unmarked: int = 7
+    recaptured_marked: int = 7
+
+    chapman_summary = aem.LincolnPetersen.chapman_unbiased_summary(
+        captured, recaptured_unmarked, recaptured_marked
+    )
+
+    assert abs(chapman_summary[aem.LincolnPetersen.estimator_id] - 164) <= 0.0000000005
+    assert (
+        abs(chapman_summary[aem.LincolnPetersen.standard_error_id] - 35.8236421003)
+        <= 0.0000000005
+    )
